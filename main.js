cb2905f8f0f40145ca56a50109f73189c2f151a2
Taylor Smith <tsmith@tsmithcreative.com>
2018-03-21 14:43:47 +0000

    Toss a table together with the results

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 xhr.open('GET', '/api-placeholder-response.json');
-xhr.onload = function () {
+xhr.onload = () => {
   if (xhr.status === 200) {
     const response = JSON.parse(xhr.responseText);
 
@@ -26,7 +26,22 @@ xhr.onload = function () {
       }
     }
 
-    console.log(report);
+    report.forEach((line) => {
+      const table = document.getElementById("report");
+      const row = table.insertRow(-1);
+
+      // So we can style coffee shop "attendees"
+      row.classList.add((line.network.indexOf("offee") > -1) ? "coffee" : "no-coffee");
+
+      // To ensure the right order, select props each instead of iterating
+      ['team', 'machine', 'network'].forEach((prop, index) => {
+        const cell = row.insertCell(index);
+
+        // Append a text node to the cell
+        const value = document.createTextNode(line[prop]);
+        cell.appendChild(value);
+      });
+    });
   } else {
     alert('API did not provide a response: HTTP' . xhr.status);
   }
