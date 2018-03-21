0dff0e6cb08fd950a4d1a8fda342d2594da1401d
Taylor Smith <tsmith@tsmithcreative.com>
2018-03-21 14:21:12 +0000

    Super simple iteration over the API response to make a 'report' array with the right columns

--- main.js
+++ main.js
@@ -2,8 +2,33 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', '/api-placeholder-response.json');
 xhr.onload = function () {
   if (xhr.status === 200) {
-    var response = JSON.parse(xhr.responseText);
-    document.getElementById('box').innerHTML = ["<pre>", JSON.stringify(response), "</pre>"].join();
+    const response = JSON.parse(xhr.responseText);
+
+    // New array to hold the filtered report:
+    const report = [];
+
+    // If this ain't quick and dirty, I don't know what is:
+    for (const team in response) {
+      if (! response.hasOwnProperty(team)) {
+        continue;
+      }
+
+      for (const computer in response[team]) {
+        if (! response[team].hasOwnProperty(computer)) {
+          continue;
+        }
+
+        let teamName = team;
+        let computerName = computer;
+        let computerNetwork = response[team][computer];
+
+        report.push({team: teamName, machine: computerName, network: computerNetwork});
+      }
+    }
+
+    console.log(report);
+  } else {
+    alert('API did not provide a response: HTTP' . xhr.status);
   }
 };
 xhr.send();
